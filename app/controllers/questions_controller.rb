9ce95fdec1b8533ca61c77a27beb03a85b124842
natalia torres <nannazinha@gmail.com>
2019-08-19 12:54:57 -0300

    coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    message = params[:question]
+
+    if message == 'I am going to work!'
+      @answer = 'Great!'
+    elsif message.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    @answer
+  end
+end
